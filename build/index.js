d7a6afc91cac5710a75ad912375f0f3b9c8728d2
ssalcedo <ssalcedo@agiasolutions.com>
2022-12-30 10:48:48 -0500

    mi primer commit2

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-_app["default"].listen(process.env.PORT || 3000);
-console.log("Server listen on port: ", process.env.PORT || 3000);
