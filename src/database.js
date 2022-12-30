a89a89161b740bad72fbe339276f85549a82504b
ssalcedo <ssalcedo@agiasolutions.com>
2022-12-30 11:12:32 -0500

    añadido

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
+require('dotenv').config();
 
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb+srv://sevastian:sev123456789@cluster0.8caypnu.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URL)
     .then(db => console.log('Db is connected'))
     .catch(error => console.log(error))
